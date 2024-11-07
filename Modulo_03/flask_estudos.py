e65b2b33bb21ab6a0af2566bf853b66a8c11008e
Gianlucca Boghi <gianboghi@hotmail.com>
2024-11-07 12:02:05 -0300

    Implementing CRUS of tasks

---
+++ Modulo_03/flask_estudos.py
@@ -0,0 +1,3 @@
+#Flask é um framework para construir aplicações web em Python
+
+#Framework é um conjunto de ferramentas, bibliotecas e convenções que fornecem uma estrutura para desenvolver aplicações
